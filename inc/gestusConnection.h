f181ebf61947faa70cb7cbd14eb8b7fac2bb426a
Oleg <o.kamenscic@gmail.com>
2017-03-12 14:35:46 +0200

    comments we need more comments

--- inc/gestusConnection.h
+++ inc/gestusConnection.h
@@ -16,7 +16,9 @@
 
 using namespace std;
 using namespace libconfig;
-
+/*
+ * structure for default bluezobject and aarameters 
+ */
 struct dbus_bluez
 {
     string name;
@@ -33,6 +35,9 @@ struct dbus_bluez
     }
 
 };
+/*
+ * structure with default names of GR devices
+ */
 struct device_names
 {
     string left;
@@ -44,6 +49,9 @@ struct device_names
     }
 
 };
+/*
+ * default GR device UUID's setted inhardware 
+ */
 struct device_uuids
 {
     string fingers;
@@ -68,7 +76,9 @@ struct device_uuids
          tmp_uuid = "6e400003-b5a3-f393-e0a9-e50e24dcca9e";
     }
 };
-
+/*
+ * structure for storing of device and adapter parameters
+ */
 struct device_t
 {
    int id;
@@ -100,43 +110,42 @@ struct device_t
 class GestusConnection
 {
     public:
-        GestusConnection();
-        ~GestusConnection();
-        GestusConnection(const GestusConnection& );
-        GestusConnection& operator=(const GestusConnection& );
+        GestusConnection(); // constructor
+        ~GestusConnection(); // destructor
+        GestusConnection(const GestusConnection& ); // copy construcor
+        GestusConnection& operator=(const GestusConnection& ); // operator =
 
 
-        bool setAdapterName(); //gets the pc bluetooth adapter name
-        string getAdapterName();
-        device_t getAdapter();
+        bool setAdapterName(); //set the nternal pc adapter parameters and stor them in object
+        string getAdapterName(); // return the name of internal adapter
+        device_t getAdapter(); // return the object adapter 
 
-        int getDeviceId(device_t);
-        string getDeviceName(device_t);
-        vector<device_t> getConnectedDevices();
+        int getDeviceId(device_t); // return the ID of device setted inparameter
+        string getDeviceName(device_t); // return the device name setted in parameter
+        vector<device_t> getConnectedDevices(); // returns vector filled with connected device structures
 
-        bool getData(int, string, deque<string>*);
-        bool connectAndRead(device_t, string, deque<string>* );
+        bool getData(int, string, deque<string>*); // fiil buffer with data taked from device wit ID from parameter and their characteristc 
+        bool connectAndRead(device_t, string, deque<string>* ); // find device in DBUS tree, connect and read data from needed characteristic 
 
-        bool setAvalibleDevices();
+        bool setAvalibleDevices(); // iterate all connected devices select the GR devices and store them 
     private:
         //private variables
-
         device_t adapter; //name of adapter in pc
         vector<device_t> devices; //names gesture input devices
         char* message;
 
         //private methods
-        bool setDeviceCharacteristics(device_t* , string);
-        DBusMessage *getProperty(const char*, const char*, const char*);
-        bool parsePropertyArray(DBusMessage*, vector<string>*);
-        bool parsePropertyString(DBusMessage*, string*);
-        bool iterDevices(DBusMessageIter*, vector<string>*, int, int);
-        bool getReply(DBusMessage* , string*);
+        bool setDeviceCharacteristics(device_t* , string); // sed default device characteristics
+        DBusMessage *getProperty(const char*, const char*, const char*); // return properties of device
+        bool parsePropertyArray(DBusMessage*, vector<string>*); // parse properties with needed from aray 
+        bool parsePropertyString(DBusMessage*, string*); // parse proerties with needed from string
+        bool iterDevices(DBusMessageIter*, vector<string>*, int, int); // iterate DBUS tree with devices
+        bool getReply(DBusMessage* , string*); // store reply as string
 
         //constants
-        dbus_bluez dbusBluez;
-        device_names deviceNames;
-        device_uuids uuids;
+        dbus_bluez dbusBluez; // DBUS bluez object
+        device_names deviceNames; // names neede for parsing
+        device_uuids uuids; // uuids of sensors
 };
 
 #endif
