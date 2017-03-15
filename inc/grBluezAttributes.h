5670e8c1ac84ec759303986fef29596cd64c9e1d
Oleg <o.kamenscic@gmail.com>
2017-03-15 14:53:16 +0200

    some decorations before writing of logs

---
+++ inc/grBluezAttributes.h
@@ -0,0 +1,100 @@
+#ifndef GR_BLUEZ_ATTRIBUTES
+#define GR_BLUEZ_ATTRIBUTES
+
+#include <iostream>
+#include <vector>
+#include <map>
+#include <deque>
+#
+/*
+ * structure for default bluezobject and aarameters 
+ */
+struct dbus_bluez
+{
+    std::string name;
+    std::string path;
+    std::string properties;
+    std::string introspectable;
+    dbus_bluez()
+    {
+        name = "org.bluez";
+        path = "/org/bluez";
+        properties = "org.freedesktop.DBus.Properties";
+        introspectable = "org.freedesktop.DBus.Introspectable";
+
+    }
+
+};
+/*
+ * structure with default names of GR devices
+ */
+struct device_names
+{
+    std::string left;
+    std::string right;
+    device_names()
+    {
+        left = "Gestus[L]";
+        right = "Gestus[R]";
+    }
+
+};
+/*
+ * default GR device UUID's setted inhardware 
+ */
+struct device_uuids
+{
+    std::string fingers;
+    std::string gyro;
+    std::string accelerometer;
+    std::string magnet;
+
+    std::string rx;
+    std::string tx;
+
+    std::string tmp_uuid;
+    device_uuids()
+    {
+         fingers = "fced6409-c015-45ea-b50d-1de32a1c2f6d";
+         gyro = "fced640a-c015-45ea-b50d-1de32a1c2f6d";
+         accelerometer = "fced640b-c015-45ea-b50d-1de32a1c2f6d";
+         magnet = "fced640c-c015-45ea-b50d-1de32a1c2f6d";
+
+         rx = "";
+         tx = "";
+
+         tmp_uuid = "6e400003-b5a3-f393-e0a9-e50e24dcca9e";
+    }
+};
+/*
+ * structure for storing of device and adapter parameters
+ */
+struct device_t
+{
+   int id;
+   std::string name;
+   std::string path;
+
+   std::map<std::string, std::string> interfaces;
+
+   std::string fingers;
+   std::string gyro;
+   std::string accelerometer;
+   std::string magnet;
+   device_t()
+   {
+       id = 0;
+       name.clear();
+       path.clear();
+       fingers.clear();
+       gyro.clear();
+       accelerometer.clear();
+       magnet.clear();
+
+       interfaces.clear();
+
+    }
+
+};
+
+#endif
