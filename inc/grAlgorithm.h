70c30ec9e2fe0e3268dce00ce42564d4d51825b8
Oleg <o.kamenscic@gmail.com>
2017-04-14 14:58:04 +0300

    clean grAlgorithm

--- inc/grAlgorithm.h
+++ inc/grAlgorithm.h
@@ -13,17 +13,9 @@
 #include <eigen3/Eigen/Geometry>
 
 
-//#include "kalman/ekfilter.hpp"
 #include <grDevice.h>
 using namespace std;
 //using namespace Eigen;
-/*
-struct imu
-{
-    int id;
-    Eigen::Quaterniond quanternion;
-};
-*/
 struct alg_device_t
 {
     // q x y z
@@ -42,72 +34,26 @@ class GRAlgorithm
 
     public:
         GRAlgorithm();
-        //GRAlgorithm(device_t device);
         ~GRAlgorithm();
         GRAlgorithm(const GRAlgorithm& );
         GRAlgorithm& operator=(const GRAlgorithm&);
-
         void grInitAlgorithms();
-
-        /*void grDCMNomalize();
-        void grDCMDriftCorrection();
-        void grDCMMatrixUpdate();
-        void grDCMEulerAngels();
-        */
-
-        //matrix multiply, vector dot product, cross product, mult vector by scalar, vector add,  are implemented,
-
-        bool imuEKF();
  //   private:
         //helper methods
         double constrain(double , double , double );
         double gravity = 256;
-        //functions for generating jacobian matrices for kalman filter
-        //DCM variables
-        //double kpRollPitch = 0.02;
-        //double kiRollPitch = 0.00002;
-        //double kpYaw = 1.2;
-        //double kiYaw = 0.00002;
 
-        //device_t device;
-        //algDev_t result;
-
-        //double magHeading;
-        /*
-        Eigen::Matrix2d DCMMatrix = new Eigen::Matrix2d(3, 3);
-
-        Eigen::Vector3d accelVector = new Eigen::Vector3d(0, 0, 0);
-        Eigen::Vector3d gyroVector = new Eigen::Vector3d(0, 0, 0);
-        Eigen::Vector3d omegaVector = Eigen::Vector3d(0, 0, 0);
-        Eigen::Vector3d omegaP = Eigen::Vector3d(0, 0, 0);
-        Eigen::Vector3d omegaI = Eigen::Vector3d(0, 0, 0);
-        Eigen::Vector3d omega = Eigen::Vector3d(0, 0, 0);
-
-        Eigen::Vector3d errorRollPitch = new Eigen::Vector3d(0, 0, 0);
-        Eigen::Vector3d errorYaw = new Eigen::Vector3d(0, 0, 0);
-        */
-        //Magwick variables
         float sampleFreq = 512.0;
         float betaDef = 0.1;
         float beta;
         float invSqrt(float x);            // algorithm gain
                                       // 2 * proportional gain (Kp)
         float q0 , q1, q2, q3;  // quaternion of sensor frame relative to auxiliary frame
-        /*void makeA();
-        void makeH();
-        void makeV();
-        void makeR();
-        void makeW();
-        void makeQ();
-        void makeProccess();
-        void makeMeasure();
-*/
-
         void MadgwickAHRSupdate(float gx, float gy, float gz, float ax, float ay, float az, float mx, float my, float mz, std::deque<std::vector<float>>*);
         void MadgwickAHRSupdateIMU(float gx, float gy, float gz, float ax, float ay, float az, std::deque<std::vector<float>>*);
 
-        void updateBuffer(imu*, std::deque<std::vector<float>>*);
-        void madgwickAHRS(device_t*, alg_device_t*);
+        void madgwickUpdateBuffer(imu*, std::deque<std::vector<float>>*);
+        void madgwickUpdateThr(device_t*, alg_device_t*);
 
 };
 
