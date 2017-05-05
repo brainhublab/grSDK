685d4c01fad3e0bf58a74e4ab5612b50bcb64a98
Oleg <o.kamenscic@gmail.com>
2017-05-05 17:41:54 +0300

    _

--- src/grAlgorithm.cpp
+++ src/grAlgorithm.cpp
@@ -261,6 +261,7 @@ float GRAlgorithm::invSqrt(float x) {
     i = 0x5f3759df - (i>>1);
     y = *(float*)&i;
     y = y * (1.5f - (halfx * y * y));
+    y = y * (1.5f - (halfx * y * y));
     return y;
 }
 
