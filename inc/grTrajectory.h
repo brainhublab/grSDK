0a19eecdb1a48bd69566bec9296a158f30c9fe53
Georgi Peev <georgimpeev@gmail.com>
2017-07-09 16:40:41 +0300

    update trajectory to use quaternion orientation

--- inc/grTrajectory.h
+++ inc/grTrajectory.h
@@ -3,8 +3,10 @@
 
 #include <vector>
 #include "Eigen/Dense"
+#include "Eigen/Geometry"
 
 using Eigen::Vector3d;
+using Eigen::Quaterniond;
 using namespace std;
 
 #define G 9.000416
@@ -19,16 +21,19 @@ class GRTrajectory
         Vector3d _getNewPosByVelocity(Vector3d, int);
         Vector3d _getNewPosByIntegrating(Vector3d, int);
 
+        Vector3d _rotateAcc(Vector3d, Quaterniond);
+
         vector<double> _toStdVector(Vector3d);
         Vector3d _toVector3d(vector<double>);
+        Quaterniond _toQuaterniond(vector<double>);
     public:
         GRTrajectory();
         ~GRTrajectory();
 
-        vector<double> getNewPosByVelocity(vector<double>, int);
-        vector<double> getNewPosByIntegrating(vector<double>, int);
+        vector<double> getNewPosByVelocity(vector<double>, vector<double>, int);
+        vector<double> getNewPosByIntegrating(vector<double>, vector<double>, int);
 
-        vector<double> getNewPos(vector<double> acc, int timestamp);
+        vector<double> getNewPos(vector<double> acc, vector<double>, int timestamp);
 };
 
 #endif
