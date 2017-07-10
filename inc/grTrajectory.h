32592f6f6a57d30bd4a8981c9ccf33af7735f635
Georgi Peev <georgimpeev@gmail.com>
2017-07-10 19:55:42 +0300

    implement veliko's trajectory algorithm

--- inc/grTrajectory.h
+++ inc/grTrajectory.h
@@ -2,6 +2,7 @@
 #define GR_TRAJECTORY
 
 #include <vector>
+#include <cmath>
 #include "Eigen/Dense"
 #include "Eigen/Geometry"
 
@@ -9,8 +10,9 @@ using Eigen::Vector3d;
 using Eigen::Quaterniond;
 using namespace std;
 
-#define G  9.80665// 9.000416 need to be 9.80665
-
+#define G  9.80665  // 9.000416 need to be 9.80665
+#define ACC_MULT 0.244
+#define FILTER_LOW 1000
 
 class GRTrajectory
 {
@@ -18,11 +20,15 @@ class GRTrajectory
         Vector3d pos_last;
         Vector3d velocity_last;
         unsigned long timestamp_last;
+        bool first_call;
 
         Vector3d _getNewPosByVelocity(Vector3d, unsigned long);
         Vector3d _getNewPosByIntegrating(Vector3d, unsigned long);
 
+        Vector3d _getNewPosByVeliko(Vector3d, unsigned long);
+
         Vector3d _rotateAcc(Vector3d, Quaterniond);
+        Vector3d _getAccValue(Vector3d);
 
         vector<double> _toStdVector(Vector3d);
         Vector3d _toVector3d(vector<double>);
@@ -33,6 +39,8 @@ class GRTrajectory
 
         vector<double> getNewPosByVelocity(vector<double>, vector<double>, unsigned long);
         vector<double> getNewPosByIntegrating(vector<double>, vector<double>, unsigned long);
+
+        vector<double> getNewPosByVeloko(vector<double>, vector<double>, unsigned long);
 
         vector<double> getNewPos(vector<double> acc, vector<double>, unsigned long);
 };
--- src/grTrajectory.cpp
+++ src/grTrajectory.cpp
@@ -3,6 +3,7 @@
 
 GRTrajectory::GRTrajectory()
 {
+    this->first_call = true;
     this->timestamp_last = 0;
     this->pos_last(0) = 0;
     this->pos_last(1) = 0;
@@ -18,13 +19,13 @@ GRTrajectory::~GRTrajectory()
 
 Vector3d GRTrajectory::_getNewPosByVelocity(Vector3d acc, unsigned long timestamp)
 {
-    std::cout<<"timestamp -->"<<timestamp<<std::endl;
+    Vector3d acc_ms, velocity, distance, pos_next;
+    double dt = (timestamp - this->timestamp_last) / 1000.f;
 
-    Vector3d velocity, distance, pos_next;
-    double time_elapsed = (timestamp - this->timestamp_last) / 1000.f;
-    std::cout<<"time elapsed -->"<<time_elapsed<<std::endl;
-    velocity = this->velocity_last + ((((acc * 0.244) / 1000) * G) * time_elapsed);
-    distance = velocity * time_elapsed;
+    acc_ms = this->_getAccValue(acc);
+
+    velocity = this->velocity_last + acc_ms * dt;
+    distance = velocity * dt;
 
     pos_next = this->pos_last + distance;
     this->velocity_last = velocity;
@@ -33,13 +34,42 @@ Vector3d GRTrajectory::_getNewPosByVelocity(Vector3d acc, unsigned long timestam
 
 Vector3d GRTrajectory::_getNewPosByIntegrating(Vector3d acc, unsigned long timestamp)
 {
-    Vector3d pos_next;
-    double time_elapsed = (timestamp - this->timestamp_last) / 1000.f;
-    std::cout<<"temp acc --->" <<((acc * 0.244) / 1000) <<std::endl;
+    Vector3d acc_ms, pos_next;
+    double dt = (timestamp - this->timestamp_last) / 1000.f;
 
-    pos_next = this->pos_last + this->velocity_last * time_elapsed + 0.5 * (((acc * 0.244) / 1000) * G) * (time_elapsed * time_elapsed);
+    acc_ms = this->_getAccValue(acc);
 
-    this->velocity_last = this->velocity_last + ((((acc * 0.244) / 1000) * G) * time_elapsed);
+    cout << "temp acc: " << acc_ms << endl;
+
+    pos_next = this->pos_last + this->velocity_last * dt + 0.5 * acc_ms * (dt * dt);
+
+    this->velocity_last = this->velocity_last + acc_ms * dt;
+    return pos_next;
+}
+
+Vector3d GRTrajectory::_getNewPosByVeliko(Vector3d acc, unsigned long timestamp)
+{
+    Vector3d pos_next, velocity_new, acc_ms;
+    double dt = (timestamp - this->timestamp_last) / 1000.f;
+
+    acc_ms = this->_getAccValue(acc);
+
+    if(this->first_call)
+    {
+        this->first_call = false;
+
+        pos_next = 0.5 * acc_ms * (dt * dt);
+        velocity_new = (pos_next - this->pos_last) / dt;
+        this->velocity_last = velocity_new;
+    }
+
+    pos_next = 0.5 * acc_ms * (dt * dt) + this->velocity_last * dt + this->pos_last;
+    velocity_new = (pos_next - this->pos_last) / dt;
+
+    this->pos_last = pos_next;
+    this->velocity_last = velocity_new;
+    this->timestamp_last = timestamp;
+
     return pos_next;
 }
 
@@ -71,6 +101,23 @@ Vector3d GRTrajectory::_rotateAcc(Vector3d acc, Quaterniond q)
 
     Quaterniond out_q = q * acc_q * q.inverse();
     Vector3d out = out_q.vec();
+    return out;
+}
+
+Vector3d GRTrajectory::_getAccValue(Vector3d in)
+{
+    Vector3d out;
+
+    for(int i = 0; i < in.rows(); i++)
+    {
+        if(abs(in(i)) < FILTER_LOW)
+        {
+            in(i) = 0;
+        }
+    }
+
+    out = ((in * ACC_MULT) / 1000) * G;
+
     return out;
 }
 
@@ -109,6 +156,27 @@ vector<double> GRTrajectory::getNewPosByIntegrating(vector<double> acc, vector<d
     pos_next = this->_getNewPosByIntegrating(acc_fixed, timestamp);
     this->pos_last = pos_next;
     this->timestamp_last = timestamp;
+
+    return this->_toStdVector(pos_next);
+}
+
+vector<double> GRTrajectory::getNewPosByVeloko(vector<double> acc, vector<double> q, unsigned long timestamp)
+{
+    if(this->timestamp_last == 0)
+    {
+        this->timestamp_last = timestamp;
+        return this->_toStdVector(this->pos_last);
+    }
+    Vector3d acc_v, acc_fixed, pos_next;
+    Quaterniond q_q;
+
+    acc_v = this->_toVector3d(acc);
+    q_q = this->_toQuaterniond(q);
+    acc_fixed = this->_rotateAcc(acc_v, q_q);
+
+    std::cout << "acc_fixed" << std::endl << acc_fixed << std::endl;
+
+    pos_next = this->_getNewPosByVeliko(acc_fixed, timestamp);
 
     return this->_toStdVector(pos_next);
 }
