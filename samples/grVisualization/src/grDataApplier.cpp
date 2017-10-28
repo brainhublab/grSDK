534ef7d3f15f7338753e57dc7f1729cb8a8cbfa1
Oleg <o.kamenscic@gmail.com>
2017-10-28 16:41:37 +0300

    temporary fixes of visualisation with 2 devices

--- samples/grVisualization/src/grDataApplier.cpp
+++ samples/grVisualization/src/grDataApplier.cpp
@@ -30,7 +30,7 @@ GRDataApplier::GRDataApplier()
     fingers["index"] = 3;
     fingers["thumb"] = 4;
 
-    alg.setupMadgwick(140, 140, 140, 140, 140, 220); //need to check
+    alg.setupMadgwick(280, 280, 280, 280, 280, 220); //need to check
 
 }
 
