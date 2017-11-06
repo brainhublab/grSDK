6ad19968019ffa6cb99bbe3a4cb9b54d355e421f
vlad <timadevelop@gmail.com>
2017-11-06 08:13:12 +0200

    Brief doc template for GR Trajectory

--- inc/grTrajectory.h
+++ inc/grTrajectory.h
@@ -22,48 +22,121 @@ struct runge_vars
     Eigen::Vector3d pos = Eigen::Vector3d(0.f, 0.f, 0.f);
     Eigen::Vector3d acc = Eigen::Vector3d(0.f, 0.f, 0.f);
 };
-
+/**
+ * GR Trajectory class descr
+ */
 class GRTrajectory
 {
     public:
-        GRTrajectory(); //constructor
-        ~GRTrajectory(); //destructor
+	/**
+	 * @brief constructor
+	 */
+        GRTrajectory(); 
+	/**
+	 * @brief destructor
+	 */
+        ~GRTrajectory();
+	/**
+	 * @brief 
+	 */
         std::vector<double> getNewPosByRunge(std::vector<double>, std::vector<double>, unsigned long);
+	/**
+	 * @brief 
+	 */
         std::vector<double> getAccelerations(std::vector<double>, std::vector<double>);
+	/**
+	 * @brief 
+	 */
         bool isStationary();
 
     private:
         /* Storing some values from previous calculations */
-        unsigned long _timestampLast;  //last stored timestamp
+
+	/**
+	 * @brief last stored timestamp 
+	 */
+        unsigned long _timestampLast;
+	/**
+	 * @brief last runge vectors
+	 */
         runge_vars _rungeLast;
 
         /* Constant values */
-        const Eigen::Vector3d _gravity = Eigen::Vector3d(0.0, 0.0, G);  //gravity vector
+	/**
+	 * @brief constant gravity vector
+	 */
+        const Eigen::Vector3d _gravity = Eigen::Vector3d(0.0, 0.0, G);
 
         /* Quaternion rotation methods */
-        Eigen::Vector3d _rotateAcc(Eigen::Vector3d, Eigen::Quaterniond);  //rotate accelerometer vector with quaternion
+	
+	/**
+	 * @brief rotate accelerometer vector with quaternion 
+	 */
+        Eigen::Vector3d _rotateAcc(Eigen::Vector3d, Eigen::Quaterniond);
 
         /* Unit conversion methods */
-        Eigen::Vector3d _convertAccToG(Eigen::Vector3d);  //coverts acceleromer raw data to G
-        Eigen::Vector3d _convertAccToMS(Eigen::Vector3d);  //coverts acceleromer raw data to G
+
+	/**
+	 * @brief coverts acceleromer raw data to G
+	 */
+        Eigen::Vector3d _convertAccToG(Eigen::Vector3d);
+	/**
+	 * @brief coverts acceleromer raw data to G
+	 */
+        Eigen::Vector3d _convertAccToMS(Eigen::Vector3d);
 
         /* Type conversion methods */
-        vector<double> _toStdVector(Eigen::Vector3d);  //convert Eigen::Vector3d to std::vector
-        Eigen::Vector3d _toVector3d(vector<double>);  //convert std::vector to Eigen::vector3d
-        Eigen::Quaterniond _toQuaterniond(vector<double>);  //convert std::vector to Eigen::Quaterniond
+	
+	/**
+	 * @brief convert Eigen::Vector3d to std::vector
+	 */
+        vector<double> _toStdVector(Eigen::Vector3d);
+	/**
+	 * @brief convert std::vector to Eigen::vector3d
+	 */
+        Eigen::Vector3d _toVector3d(vector<double>);
+	/**
+	 * @brief convert std::vector to Eigen::Quaterniond
+	 */
+        Eigen::Quaterniond _toQuaterniond(vector<double>);
 
         /* Integration calculation methods */
-        runge_vars _rk4(Eigen::Vector3d, runge_vars, double );  // Runge-Kutta method
 
+	/**
+	 * @brief Runge-Kutta method
+	 */
+        runge_vars _rk4(Eigen::Vector3d, runge_vars, double );
+
+	/**
+	 * @brief 
+	 */
         Eigen::Vector3d _correctVector(Eigen::Vector3d);
 
+	/**
+	 * @brief 
+	 */
         Eigen::Vector3d _accLast;
 
+	/**
+	 * @brief describes if current acc is stationary  
+	 */
         bool _isStationary;
 
+	/**
+	 * @brief 
+	 */
         double _accTreshold;
+	/**
+	 * @brief 
+	 */
         double _gyroTreshold;
+	/**
+	 * @brief 
+	 */
         double _varianceTreshold;
+	/**
+	 * @brief 
+	 */
         std::queue<int> _stationaryWindow;
 };
 
